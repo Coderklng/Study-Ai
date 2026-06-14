ab473737027d87bff1914f95d2a3588a14167a74
Coderklng <kkartiksharma999@gmail.com>
2026-06-14 13:50:08 +0530

    Google AdSense metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 import { ThemeProvider } from "../../Components/context/ThemeContext";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +26,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <Script async src="[https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1752210907408525](https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1752210907408525)" crossOrigin="anonymous" strategy="afterInteractive" />
-      <body className="min-h-full flex flex-col">
+       <body className="min-h-full flex flex-col">
       <ThemeProvider>
         {children}
       </ThemeProvider>
