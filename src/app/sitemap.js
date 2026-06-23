cc3f053606a5bfb1b7e8dd96c0a67cdeb077d87d
Coder_Kartik <kkartiksharma999@gmail.com>
2026-06-23 16:29:35 +0530

    Add sitemap function to generate site map

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://study-ai-lilac-delta.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+  ]
+}
